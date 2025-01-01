fc602c2fe15c3f43d914b319ae77984cdd28abae
Lyan <lyanbrito613@gmail.com>
2025-01-01 18:06:43 -0300

    feat:create-global-style

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './components/header/index.jsx';
+import { AppContainer } from './styles.js';
+
+function App() {
+  return (
+    <AppContainer>
+      <Header />
+    </AppContainer>
+  );
+}
+
+export default App;
