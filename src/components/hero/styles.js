a5dcbc16e642a6bdd086d7402fda33eb2e047644
Lyan <lyanbrito613@gmail.com>
2025-01-02 21:14:16 -0300

    feat:created-hero-container

---
+++ src/components/hero/styles.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+
+export const HeroContainer = styled.div`
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    background-color: #F4F6F5;
+    border-radius: 59px;
+    gap: 43px;
+    width: 89.59%;
+    height: 852px;
+    `
+export const TextWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 83px;
+    width: 606px;
+    gap: 36px;
+    `
+export const HeroTitle = styled.h1`
+    font: 900 96px "Poppins";       
+    line-height: 120px;
+    width: 474px; 
+    `
+export const HeroImage = styled.img`
+    width: 784px;
+    `
+export const HeroText = styled.p`
+    font: 300 28px "Poppins";
+    `
+export const HeroButton = styled.button`
+    font: 300 30px "Poppins";
+    background-color: black;
+    color: white;
+    padding: 13px 22px;
+    border-radius: 7px;
+    font-size: 24px;
+    border: none;
+    width: fit-content;
+    `
+    export const HeroElement = styled.img`
+    position: absolute;
+    top: 243px;
+    left: 181px;
+
+    `
