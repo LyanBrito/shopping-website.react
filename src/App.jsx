86019a1907cede1efa55884f99126f4e75b9b46e
Lyan <lyanbrito613@gmail.com>
2025-01-07 22:58:12 -0300

    feat:created-carousel-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import Header from './components/header/index.jsx';
-import Hero from './components/hero/index.jsx';
-import { AppContainer } from './styles.js';
+import Carousel from "./components/carousel/index.jsx";
+import Header from "./components/header/index.jsx";
+import Hero from "./components/hero/index.jsx";
+import { AppContainer } from "./styles.js";
 
 function App() {
-  return (
-    <AppContainer>
-      <Header />
-      <Hero />
-    </AppContainer>
-  );
+	return (
+		<AppContainer>
+			<Header />
+			<Hero />
+			<Carousel />
+		</AppContainer>
+	);
 }
 
 export default App;
