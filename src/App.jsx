357decf0e76d14ad15f65b0a6a499485437e2ad0
Lyan <lyanbrito613@gmail.com>
2025-01-10 21:52:41 -0300

    feat:added-favourites-&-wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import Arrivals from "./components/arrivals/index.jsx";
+// import Arrivals from "./components/arrivals/index.jsx";
+// import Carousel from "./components/carousel/index.jsx";
+// import Header from "./components/header/index.jsx";
+// import Hero from "./components/hero/index.jsx";
+
 import Banner from "./components/banner/index.jsx";
-import Carousel from "./components/carousel/index.jsx";
-import Header from "./components/header/index.jsx";
-import Hero from "./components/hero/index.jsx";
+import Favourites from "./components/favourites/index.jsx";
+import HeroWrapper from "./components/hero-wrapper/index.jsx";
 import { AppContainer } from "./styles.js";
 
 function App() {
 	return (
 		<AppContainer>
+			{/* <>
 			<Header />
 			<Hero />
 			<Carousel />
 			<Arrivals />
+			</> */}
+			<HeroWrapper />
 			<Banner />
+			<Favourites />
 		</AppContainer>
 	);
 }
