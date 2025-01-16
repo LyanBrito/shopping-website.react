a35d6c06bfa5d092af5d6501a15fc386b572f306
Lyan <lyanbrito613@gmail.com>
2025-01-15 21:16:37 -0300

    chore:formated-files

--- src/components/favourites/index.jsx
+++ src/components/favourites/index.jsx
@@ -1,57 +1,55 @@
 import {
-    GirlsVibing,
-    GirlWithGlassesVibing,
-    RightArrow,
+	GirlWithGlassesVibing,
+	GirlsVibing,
+	RightArrow,
 } from "../../assets/svg/export";
 import {
-    FavouritesSection,
-    FavouritesTitle,
-    ArrowImage,
-    Card,
-    CardButton,
-    CardDescription,
-    CardImage,
-    Cards,
-    DescriptionSubTitle,
-    DescriptionText,
-    DescriptionTitle,
-    TitleH3,
+	ArrowImage,
+	Card,
+	CardButton,
+	CardDescription,
+	CardImage,
+	Cards,
+	DescriptionSubTitle,
+	DescriptionText,
+	DescriptionTitle,
+	FavouritesSection,
+	FavouritesTitle,
+	TitleH3,
 } from "./styles";
 
 export default function Favourites() {
-    return (
-        <FavouritesSection>
-            <FavouritesTitle>
-                <TitleH3>
-                    Young's Favourite
-                </TitleH3>
-            </FavouritesTitle>
-            <Cards>
-                <Card>
-                    <CardImage src={GirlsVibing} alt="girl-putting-cloath" />
-                    <CardDescription>
-                        <DescriptionText>
-                            <DescriptionTitle>Trending on instagram</DescriptionTitle>
-                            <DescriptionSubTitle>Explore Now!</DescriptionSubTitle>
-                        </DescriptionText>
-                        <CardButton>
-                            <ArrowImage src={RightArrow} alt="right-arrow" />
-                        </CardButton>
-                    </CardDescription>
-                </Card>
-                <Card>
-                    <CardImage src={GirlWithGlassesVibing} alt="girl-using-phone" />
-                    <CardDescription>
-                        <DescriptionText>
-                            <DescriptionTitle>All Under $40</DescriptionTitle>
-                            <DescriptionSubTitle>Explore Now!</DescriptionSubTitle>
-                        </DescriptionText>
-                        <CardButton>
-                            <ArrowImage src={RightArrow} alt="right-arrow" />
-                        </CardButton>
-                    </CardDescription>
-                </Card>
-            </Cards>
-        </FavouritesSection>
-    );
+	return (
+		<FavouritesSection>
+			<FavouritesTitle>
+				<TitleH3>Young's Favourite</TitleH3>
+			</FavouritesTitle>
+			<Cards>
+				<Card>
+					<CardImage src={GirlsVibing} alt="girl-putting-cloath" />
+					<CardDescription>
+						<DescriptionText>
+							<DescriptionTitle>Trending on instagram</DescriptionTitle>
+							<DescriptionSubTitle>Explore Now!</DescriptionSubTitle>
+						</DescriptionText>
+						<CardButton>
+							<ArrowImage src={RightArrow} alt="right-arrow" />
+						</CardButton>
+					</CardDescription>
+				</Card>
+				<Card>
+					<CardImage src={GirlWithGlassesVibing} alt="girl-using-phone" />
+					<CardDescription>
+						<DescriptionText>
+							<DescriptionTitle>All Under $40</DescriptionTitle>
+							<DescriptionSubTitle>Explore Now!</DescriptionSubTitle>
+						</DescriptionText>
+						<CardButton>
+							<ArrowImage src={RightArrow} alt="right-arrow" />
+						</CardButton>
+					</CardDescription>
+				</Card>
+			</Cards>
+		</FavouritesSection>
+	);
 }
