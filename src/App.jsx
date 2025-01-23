a2af7e9f1782d8281f4dae07f1e505fc15e8019a
Lyan <lyanbrito613@gmail.com>
2025-01-22 22:01:40 -0300

    feat:added=footer-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-// import Arrivals from "./components/arrivals/index.jsx";
-// import Carousel from "./components/carousel/index.jsx";
-// import Header from "./components/header/index.jsx";
-// import Hero from "./components/hero/index.jsx";
-
 import Banner from "./components/banner/index.jsx";
 import Community from "./components/community/index.jsx";
 import Favourites from "./components/favourites/index.jsx";
+import PageFooter from "./components/footer/index.jsx";
 import HeroWrapper from "./components/hero-wrapper/index.jsx";
 import VoucherComponent from "./components/voucher/index.jsx";
 import { AppContainer } from "./styles.js";
@@ -13,17 +9,12 @@ import { AppContainer } from "./styles.js";
 function App() {
 	return (
 		<AppContainer>
-			{/* <>
-			<Header />
-			<Hero />
-			<Carousel />
-			<Arrivals />
-			</> */}
 			<HeroWrapper />
 			<Banner />
 			<Favourites />
 			<VoucherComponent />
 			<Community />
+			<PageFooter />
 		</AppContainer>
 	);
 }
