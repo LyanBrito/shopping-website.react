0761e8fcbd859427d0e91526776c7364cea657d1
Lyan <lyanbrito613@gmail.com>
2025-01-22 22:01:55 -0300

    chore:formated-file

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,56 +1,66 @@
 import {
-    FaceIcon,
-    InstaIcon,
-    LinkedinIcon,
-    TwitterIcon
-} from "../../assets/svg/export"
+	FaceIcon,
+	InstaIcon,
+	LinkedinIcon,
+	TwitterIcon,
+} from "../../assets/svg/export";
 
 import {
-    FooterContainer,
-    TitleFooter,
-    FooterText,
-    ButtonWrapper,
-    CompanyWrapper,
-    QuickWrapper,
-    LegalWrapper,
-    FooterLogo,
-    InfoTitle,
-    FooterButton,
+	FooterContainer,
+	TitleFooter,
+	FooterText,
+	ButtonWrapper,
+	CompanyWrapper,
+	QuickWrapper,
+	LegalWrapper,
+	FooterLogo,
+	InfoTitle,
+	FooterButton,
+    InfoWrapper,
 } from "./styles";
 
-export default function Footer() {
-    return (
-        <FooterContainer>
-            <TitleFooter>
-                <FooterLogo>FASHION</FooterLogo>
-                <FooterText>About</FooterText>
-                <ButtonWrapper>
-                    <FooterButton type="button"><img src={FaceIcon} alt="face-icon" /></FooterButton>
-                    <FooterButton type="button"><img src={InstaIcon} alt="insta-icon" /></FooterButton>
-                    <FooterButton type="button"><img src={TwitterIcon} alt="twitter-icon" /></FooterButton>
-                    <FooterButton type="button"><img src={LinkedinIcon} alt="linkedin-icon" /></FooterButton>
-                </ButtonWrapper>
-            </TitleFooter>
-            <CompanyWrapper>
-                <InfoTitle>Company</InfoTitle>
-                <FooterText>About</FooterText>
-                <FooterText>About</FooterText>
-                <FooterText>About</FooterText>
-                <FooterText>About</FooterText>
-            </CompanyWrapper>
-            <QuickWrapper>
-                <InfoTitle>Quick Link</InfoTitle>
-                <FooterText>About</FooterText>
-                <FooterText>About</FooterText>
-                <FooterText>About</FooterText>
-                <FooterText>About</FooterText>
-            </QuickWrapper>
-            <LegalWrapper>
-                <InfoTitle>Link</InfoTitle>
-                <FooterText>About</FooterText>
-                <FooterText>About</FooterText>
-            </LegalWrapper>
-            About
-        </FooterContainer>
-    );
+export default function PageFooter() {
+	return (
+		<FooterContainer>
+			<TitleFooter>
+				<FooterLogo>FASHION</FooterLogo>
+				<FooterText href="#">Complete your style with awesome clothes from us.</FooterText>
+				<ButtonWrapper>
+					<FooterButton type="button">
+						<img src={FaceIcon} alt="face-icon" />
+					</FooterButton>
+					<FooterButton type="button">
+						<img src={InstaIcon} alt="insta-icon" />
+					</FooterButton>
+					<FooterButton type="button">
+						<img src={TwitterIcon} alt="twitter-icon" />
+					</FooterButton>
+					<FooterButton type="button">
+						<img src={LinkedinIcon} alt="linkedin-icon" />
+					</FooterButton>
+				</ButtonWrapper>
+			</TitleFooter>
+            <InfoWrapper>
+			<CompanyWrapper>
+				<InfoTitle>Company</InfoTitle>
+				<FooterText href="#">About</FooterText>
+				<FooterText href="#">Contact us</FooterText>
+				<FooterText href="#">Support</FooterText>
+				<FooterText href="#">Careers</FooterText>
+			</CompanyWrapper>
+			<QuickWrapper>
+				<InfoTitle>Quick Link</InfoTitle>
+				<FooterText href="#">Share Location</FooterText>
+				<FooterText href="#">Orders Tracking</FooterText>
+				<FooterText href="#">Size Guide</FooterText>
+				<FooterText href="#">FAQs</FooterText>
+			</QuickWrapper>
+			<LegalWrapper>
+				<InfoTitle>Link</InfoTitle>
+				<FooterText href="#">Terms & conditions</FooterText>
+				<FooterText href="#">Privacy Policy</FooterText>
+			</LegalWrapper>
+            </InfoWrapper>
+		</FooterContainer>
+	);
 }
